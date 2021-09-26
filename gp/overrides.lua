76b3bc7b6d7493265666d9c305997a0b08227805
David Bethune <david@mimix.io>
2021-09-26 01:49:39 -0500

    Allow isMoveableWhenBuilt

--- gp/overrides.lua
+++ gp/overrides.lua
@@ -12,28 +12,30 @@ GP:log("overrides.lua", GP:version())
 -- FUNCTIONAL, GAME EFFECT
 function GP:override(partId)
 
-        -- Build this part's entry for the list.
-        local onePartEntry = {
-            BuildingPart = partId
-        }
+    -- Build this part's entry for the list.
+    local onePartEntry = {BuildingPart = partId}
 
-        -- Setup for random ID. Creates unique IDs for each registration.
-        local randomValue = "_" .. math.random(1,100000000)
-        local randomId = GP:magicWords().part.overrides .. randomValue
+    -- Setup for random ID. Creates unique IDs for each registration.
+    local randomValue = "_" .. math.random(1, 100000000)
+    local randomId = GP:magicWords().part.overrides .. randomValue
 
-    -- Call Foundation's override
+    -- Register a cost list for this part with no required resources.
     myMod:register({
-        DataType = GP:datatypes().part.costList, 
+        DataType = GP:datatypes().part.costList,
         Id = randomId,
-        BuildingPartCostList = {onePartEntry},
+        BuildingPartCostList = {onePartEntry}
     })
 
-    -- OVERRIDE Building Part Costs
+    -- Override the game's balancing rules with this new empty cost/resource list.
     myMod:override({
         Id = GP:datatypes().override.balancing,
-        BuildingCostOverrideList = {
-            randomId
-        },
+        BuildingCostOverrideList = {randomId}
+    })
+
+    -- Override the part's moveable.
+    myMod:override({
+        Id = partId,
+        IsMovableWhenBuilt = true
     })
 
     -- No returns. Function is called only for side effects.
